7494ea09942e34aa4cbde64b8ca43fbcbd57aeb8
CHASSERIEAU Quentin <qchasserieau@systra.info>
2018-11-28 17:25:53 +0100

    pip installable

---
+++ docs/README.rst
@@ -0,0 +1,7 @@
+# quetzal
+## What is it?
+**quetzal** is a Python package providing flexible models for transport planning and traffic forecasting.
+## Copyright 
+(c) SYSTRA
+## License
+[CeCILL-B](LICENSE)
---
+++ setup.py
@@ -0,0 +1,147 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='quetzal',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1',
+
+    description='Home-grown modules for python',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://gitlab.systra.info/probin/quetzal',
+
+    # Author details
+    author='SYSTRA',
+    author_email='qchasserieau@systra.com',
+
+    # Choose your license
+    license='RESERVED',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Other/Proprietary License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='traffic assignment visualization models',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'alabaster>=0.7.8',
+        'aniso8601>=1.1.0',
+        'argcomplete>=0.8.1',
+        'astropy>=0.4.2',
+        'Babel>=2.3.4',
+        'backports-abc>=0.4',
+        'beautifulsoup4>=4.3.2',
+        'bitarray>=0.8.1',
+        'branca',
+        'DateTime',
+        'dbf',
+        'descartes',
+        'folium',
+        'geopandas',
+        'geopy',
+        'ipywidgets',
+        'kml2geojson',
+        'networkx',
+        'numpy',
+        'pandas',
+        'pyPdf',
+        'pyproj',
+        'requests',
+        'scikit-learn',
+        'scipy',
+        'selenium',
+        'shapely',
+        'simpledbf',
+        'Sphinx>=1.4.5',
+        'sphinx-rtd-theme>=0.1.9',
+        'xlrd>=1.0.0',
+        'tables>=3.4.2',
+        'tqdm'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytests>=3'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest', 'psycogp2'],
+        'test': ['coverage', 'pytest'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+)
