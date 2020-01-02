dd560df00142f4db40a5ab713f5d70e91d63d656
CHASSERIEAU Quentin <qchasserieau@systra.info>
2020-01-02 16:37:37 +0100

    update windows install to avoid SSL fail

--- windows-install.bat
+++ windows-install.bat
@@ -1,5 +1,6 @@
 @echo off
 cd "%~dp0"
+
 if not "X%CONDA_DEFAULT_ENV%" == "Xquetzal_env" (
   conda info -e | findstr quetzal_env > NUL
   if errorlevel 1 (
@@ -8,9 +9,15 @@ if not "X%CONDA_DEFAULT_ENV%" == "Xquetzal_env" (
   )
   call activate quetzal_env
 )
+
 echo Installing...
+call set SSL_NO_VERIFY=1
 set conda_cmd=
 for /f "delims=" %%a in ('python get_conda_install_args.py') do set conda_cmd=%%a
+
 @echo on
+call conda install -y ipykernel
+call conda install -y pytables
+call conda install -y -c conda-forge geopandas
 call %conda_cmd%
-pip install -e .
+pip install -e . --trusted-host pypi.org --trusted-host files.pythonhosted.org
