1a0a9079c4b9798a4ab6acbcb7ed6bd15e6bc3d7
CHASSERIEAU Quentin <qchasserieau@systra.info>
2020-02-20 15:21:16 +0100

    change excel and gtfs importer

--- quetzal/io/excel.py
+++ quetzal/io/excel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def read_var(file='parameters.xlsx', scenario='base'):
     parameter_frame = pd.read_excel(file)
-    parameter_frame.drop('description', axis=1, errors='ignore', inplace=True)
+    parameter_frame.drop(['description', 'unit'], axis=1, errors='ignore', inplace=True)
     parameter_frame.set_index(['category','parameter'], inplace=True)
     for c in parameter_frame.columns:
         parent = parameter_frame[c][('general', 'parent')]
