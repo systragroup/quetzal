981358f8456612bdef0ad67c81db30c5ded79305
Robin Goix <rgoix@systra.com>
2021-04-08 16:22:05 +0200

    fix plot_car_path

--- quetzal/model/plotmodel.py
+++ quetzal/model/plotmodel.py
@@ -118,17 +118,23 @@ class PlotModel(summarymodel.SummaryModel):
         paths = self.car_los.set_index(['origin', 'destination']).loc[origin, destination]
         if paths.ndim == 1: # their is only one path 
             paths = pd.DataFrame(data=paths).T 
-            
+        def build_road_path(p, lp):
+            if len(p):
+                rp = p[:2]
+                rp += lp
+                rp += p[-2:]
+                return rp
+            else:
+                return lp
+        paths['road_path'] = paths[['path', 'link_path']].apply(
+            lambda x: build_road_path(x['path'], x['link_path']), 1
+        )
         # the path is added to the ax
-        for p in tqdm(list(paths['path'])):
+        for p in tqdm(list(paths['road_path'])):
             ax = plot_one_path(p, styles, ax=ax)
             ax.set_xticks([])
             ax.set_yticks([])
 
-        for p in tqdm(list(paths['link_path'])):
-            ax = plot_one_path(p, styles, ax=ax)
-            ax.set_xticks([])
-            ax.set_yticks([])
 
         if basemap_url is not None:
             assert self.epsg == 3857
