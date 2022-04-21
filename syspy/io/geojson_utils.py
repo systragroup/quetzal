fb725cf94e12e0a05d127305c68281ed11654da1
Robin Goix <rgoix@systra.com>
2022-04-21 13:39:07 +0200

    geojson export encoding

--- syspy/io/geojson_utils.py
+++ syspy/io/geojson_utils.py
@@ -7,11 +7,11 @@ default_crs_string = "urn:ogc:def:crs:EPSG::2154"
 default_prj_file = "../syspy_utils/gis_resources/epsg2154.prj"
 
 
-def set_geojson_crs(file, crs_string=default_crs_string):
-    with open(file, 'r') as infile:
+def set_geojson_crs(file, crs_string=default_crs_string, encoding='utf-8'):
+    with open(file, 'r', encoding=encoding) as infile:
         data = json.load(infile)
         infile.close()
-    with open(file, 'w') as outfile:
+    with open(file, 'w', encoding=encoding) as outfile:
         data['crs'] = {"type": "name", "properties": {"name": crs_string}}
         json.dump(data, outfile)
 
@@ -21,10 +21,10 @@ def set_shp_crs(file, prj_file=default_prj_file):
     copyfile(prj_file, file_without_extension + r'.prj')
 
 
-def gdf_to_geojson(gdf, filename, crs_string=default_crs_string, **kwargs):
+def gdf_to_geojson(gdf, filename, crs_string=default_crs_string, encoding='utf-8'):
     try:
         os.remove(filename)
     except OSError:
         pass
-    gdf.to_file(filename, driver='GeoJSON', **kwargs)
-    set_geojson_crs(filename, crs_string)
+    gdf.to_file(filename, driver='GeoJSON', encoding=encoding)
+    set_geojson_crs(filename, crs_string, encoding=encoding)
