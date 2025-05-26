42356f318f7c35c24f19575c44b2400d2ce6712b
Simon Boivin <sboivin@systra.com>
2025-05-26 13:38:56 -0400

    add some frank-wolfe tests

---
+++ tests/test_road_frank_wolfe.py
@@ -0,0 +1,129 @@
+import unittest
+
+from quetzal.engine.road_pathfinder import init_network, init_volumes, msa_roadpathfinder, extended_roadpathfinder
+import pandas as pd
+from quetzal.model import stepmodel
+
+
+zones = pd.DataFrame({'id': ['p', 'q']})
+zones.index = zones['id']
+zones.index.name = 'index'
+
+volumes = pd.DataFrame({'origin': ['p'], 'destination': ['q'], 'car': [1000]})
+volumes.index.name = 'index'
+
+zone_to_road = pd.DataFrame({'a': ['p', 'b'], 'b': ['a', 'q'], 'time': [10, 10], 'length': [10, 10]})
+zone_to_road.index = 'zr_' + zone_to_road.index.astype(str)
+zone_to_road.index.name = 'index'
+
+
+rlinks = pd.DataFrame(
+    {
+        'a': ['a', 'm1', 'a', 'm2', 'a', 'm3'],
+        'b': ['m1', 'b', 'm2', 'b', 'm3', 'b'],
+        'time': [10, 10, 20, 20, 25, 25],
+        'length': [10, 10, 10, 10, 10, 10],
+        'capacity': [200, 200, 400, 400, 300, 300],
+    }
+)
+
+rlinks.index = 'rlink_' + rlinks.index.astype(str)
+rlinks.index.name = 'index'
+
+
+num_cores = 1
+
+free_flow = 'time'
+default_bpr = 'time * (1 + {alpha} * (flow/capacity)**{beta})'.format(alpha=0.15, beta=4)
+vdf = {'free_flow': free_flow, 'default_bpr': default_bpr}
+
+
+class TestFrankWolfe(unittest.TestCase):
+    def setUp(self):
+        sm = stepmodel.StepModel()
+        rlinks['vdf'] = 'default_bpr'
+        sm.road_links = rlinks
+
+        sm.zone_to_road = zone_to_road
+        sm.zones = zones
+        sm.volumes = volumes
+        self.sm = sm
+
+    def _get_msa_roadpathfinder(self, maxiters=10, method='bfw'):
+        tolerance = 10e-6
+        segments = ['car']
+        time_column = 'time'
+        access_time = 'time'
+        network = init_network(self.sm, method, segments, time_column, access_time)
+        volumes = init_volumes(self.sm)
+        links, car_los, relgap_list = msa_roadpathfinder(
+            network,
+            volumes,
+            segments=segments,
+            method=method,
+            vdf=vdf,
+            maxiters=maxiters,
+            tolerance=tolerance,
+            time_col=time_column,
+            num_cores=num_cores,
+            log=False,
+        )
+        return links, car_los, relgap_list
+
+    def _test_msa_pathfinder_with_method(self, method):
+        links, car_los, relgap = self._get_msa_roadpathfinder(method=method)
+        expected_flows = {('a', 'm1'): 358.329, ('a', 'm2'): 464.514, ('a', 'm3'): 177.157}
+        expected_jam_time = 25.456
+        for key, expected_flow in expected_flows.items():
+            self.assertAlmostEqual(links.loc[key, 'flow'], expected_flow, places=0)
+            self.assertAlmostEqual(links.loc[key, 'jam_time'], expected_jam_time, places=1)
+
+    def test_bfw_pathfinder(self):
+        self._test_msa_pathfinder_with_method(method='bfw')
+
+    def test_fw_pathfinder(self):
+        self._test_msa_pathfinder_with_method(method='fw')
+
+    @unittest.skip('msa will fail affectation cause its not good')
+    def test_msa_pathfinder(self):
+        self._test_msa_pathfinder_with_method(method='msa')
+
+    def _get_extended_roadpathfinder(self, maxiters=10, method='bfw'):
+        tolerance = 10e-6
+        segments = ['car']
+        time_column = 'time'
+        access_time = 'time'
+        network = init_network(self.sm, method, segments, time_column, access_time)
+        volumes = init_volumes(self.sm)
+        links, car_los, relgap_list = extended_roadpathfinder(
+            network,
+            volumes,
+            zones=self.sm.zones,
+            segments=segments,
+            method=method,
+            vdf=vdf,
+            maxiters=maxiters,
+            tolerance=tolerance,
+            time_col=time_column,
+            num_cores=num_cores,
+            log=False,
+        )
+        return links, car_los, relgap_list
+
+    def _test_extended_pathfinder_with_method(self, method):
+        links, car_los, relgap = self._get_extended_roadpathfinder(method=method)
+        expected_flows = {('a', 'm1'): 358.329, ('a', 'm2'): 464.514, ('a', 'm3'): 177.157}
+        expected_jam_time = 25.456
+        for key, expected_flow in expected_flows.items():
+            self.assertAlmostEqual(links.loc[key, 'flow'], expected_flow, places=0)
+            self.assertAlmostEqual(links.loc[key, 'jam_time'], expected_jam_time, places=1)
+
+    def test_bfw_extended_pathfinder(self):
+        self._test_extended_pathfinder_with_method(method='bfw')
+
+    def test_fw_extended_pathfinder(self):
+        self._test_extended_pathfinder_with_method(method='fw')
+
+    @unittest.skip('msa will fail affectation cause its not good')
+    def test_msa_extended_pathfinder(self):
+        self._test_msa_pathfinder_with_method(method='msa')
---
+++ tests/test_road_pathfinder.py
@@ -0,0 +1,255 @@
+import unittest
+
+from quetzal.engine.road_pathfinder import (
+    init_network,
+    init_volumes,
+    aon_roadpathfinder,
+    get_car_los_time,
+    extended_roadpathfinder,
+    msa_roadpathfinder,
+)
+import pandas as pd
+from quetzal.model import stepmodel
+
+
+zones = pd.DataFrame({'id': ['z1', 'z2']})
+zones.index = zones['id']
+zones.index.name = 'index'
+
+volumes = pd.DataFrame({'origin': ['z1', 'z2'], 'destination': ['z2', 'z1'], 'car': [150, 100]})
+volumes.index.name = 'index'
+
+zone_to_road = pd.DataFrame(
+    {
+        'a': ['z1', 'a', 'e', 'z2', 'z2'],
+        'b': ['a', 'z1', 'z2', 'e', 'c'],
+        'time': [100, 100, 100, 100, 100],
+        'length': [10, 10, 10, 10, 10],
+        'direction': ['access', 'access', 'eggress', 'eggress', 'access'],
+    }
+)
+zone_to_road.index = 'zr_' + zone_to_road.index.astype(str)
+zone_to_road.index.name = 'index'
+
+
+rlinks = pd.DataFrame(
+    {
+        'a': ['a', 'b', 'b', 'c', 'b', 'd', 'd', 'e', 'e', 'c'],
+        'b': ['b', 'a', 'c', 'b', 'd', 'b', 'e', 'd', 'c', 'e'],
+        'time': [1, 1, 5, 5, 2, 2, 4, 4, 6, 6],
+        'length': [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+    }
+)
+
+rlinks.index = 'rlink_' + rlinks.index.astype(str)
+rlinks.index.name = 'index'
+
+from quetzal.engine.vdf import default_bpr, free_flow
+
+num_cores = 1
+vdf = {'free_flow': free_flow, 'default_bpr': default_bpr}
+
+
+class TestRoadPathfinder(unittest.TestCase):
+    def setUp(self):
+        sm = stepmodel.StepModel()
+        rlinks['capacity'] = 50
+        rlinks['vdf'] = 'free_flow'
+        sm.road_links = rlinks
+
+        sm.zone_to_road = zone_to_road
+        sm.zones = zones
+        sm.volumes = volumes
+        self.sm = sm
+
+    def test_init_network(self):
+        method = 'bfw'
+        segments = []
+        time_column = 'time'
+        access_time = 'time'
+        ntleg_penalty = 100
+        network = init_network(self.sm, method, segments, time_column, access_time, ntleg_penalty)
+
+        expected_len = len(self.sm.road_links) + len(self.sm.zone_to_road)
+        self.assertEqual(len(network), expected_len)
+
+        expected_columns = ['a', 'b', 'vdf', 'segments', 'direction', 'flow', 'auxiliary_flow', 'base_flow']
+        for col in expected_columns:
+            self.assertIn(col, network.columns)
+
+    def test_init_network_aon(self):
+        method = 'aon'
+        segments = []
+        time_column = 'time'
+        access_time = 'time'
+        ntleg_penalty = 100
+        network = init_network(self.sm, method, segments, time_column, access_time, ntleg_penalty)
+        expected_len = len(self.sm.road_links) + len(self.sm.zone_to_road)
+        self.assertEqual(len(network), expected_len)
+
+        expected_columns = ['a', 'b', 'time']
+        for col in expected_columns:
+            self.assertIn(col, network.columns)
+
+    def test_init_volumes(self):
+        volumes = init_volumes(self.sm)
+        pd.testing.assert_frame_equal(volumes, self.sm.volumes, check_dtype=False)
+
+    def test_init_volumes_return_volumes_if_no_volumes(self):
+        sm = self.sm.copy()
+        del sm.volumes
+        volumes = init_volumes(sm)
+        expected_len = len(sm.zones) * len(sm.zones)
+        self.assertEqual(len(volumes), expected_len)
+        self.assertIn('volume', volumes.columns)
+
+    def _get_aon_los(self):
+        method = 'aon'
+        segments = []
+        time_column = 'time'
+        access_time = 'time'
+        ntleg_penalty = 100
+        network = init_network(self.sm, method, segments, time_column, access_time, ntleg_penalty)
+        volumes = init_volumes(self.sm)
+        car_los = aon_roadpathfinder(network, volumes, time_column, num_cores)
+        return car_los
+
+    def test_aon_pathfinder(self):
+        car_los = self._get_aon_los()
+
+        expected_path_1 = ['z1', 'a', 'b', 'd', 'e', 'z2']
+        self.assertEqual(car_los['path'][0], expected_path_1)
+
+        expected_path_2 = ['z2', 'c', 'b', 'a', 'z1']
+        self.assertEqual(car_los['path'][1], expected_path_2)
+
+    def test_get_car_los_time(self):
+        car_los = self._get_aon_los()
+        car_los = get_car_los_time(car_los, self.sm.road_links, self.sm.zone_to_road, 'time', 'time')
+
+        self.assertEqual(car_los['time'][0], 207)
+        self.assertEqual(car_los['time'][1], 206)
+
+    def _get_msa_roadpathfinder(self, maxiters=10, method='bfw', track_links_list=[]):
+        tolerance = 0.01
+        segments = ['car']
+        time_column = 'time'
+        access_time = 'time'
+        ntleg_penalty = 100
+        network = init_network(self.sm, method, segments, time_column, access_time, ntleg_penalty)
+        volumes = init_volumes(self.sm)
+        links, car_los, relgap_list = msa_roadpathfinder(
+            network,
+            volumes,
+            segments=segments,
+            method=method,
+            vdf=vdf,
+            maxiters=maxiters,
+            tolerance=tolerance,
+            track_links_list=track_links_list,
+            log=False,
+            time_col=time_column,
+            num_cores=num_cores,
+        )
+        return links, car_los, relgap_list
+
+    def test_msa_pathfinder(self):
+        for method in ['bfw', 'msa', 'fw']:
+            links, car_los, relgap = self._get_msa_roadpathfinder()
+            expected_path_1 = ['z1', 'a', 'b', 'd', 'e', 'z2']
+            self.assertEqual(car_los['path'][0], expected_path_1)
+
+            expected_path_2 = ['z2', 'c', 'b', 'a', 'z1']
+            self.assertEqual(car_los['path'][1], expected_path_2)
+
+    def _get_extended_roadpathfinder(self, track_links_list=[]):
+        method = 'bfw'
+        maxiters = 10
+        tolerance = 0.01
+        segments = ['car']
+        time_column = 'time'
+        access_time = 'time'
+        ntleg_penalty = 100
+        network = init_network(self.sm, method, segments, time_column, access_time, ntleg_penalty)
+        volumes = init_volumes(self.sm)
+        links, car_los, relgap_list = extended_roadpathfinder(
+            network,
+            volumes,
+            self.sm.zones,
+            segments=segments,
+            method=method,
+            maxiters=maxiters,
+            tolerance=tolerance,
+            vdf=vdf,
+            track_links_list=track_links_list,
+            log=False,
+            time_col=time_column,
+            zone_penalty=ntleg_penalty,
+            num_cores=num_cores,
+        )
+        return links, car_los, relgap_list
+
+    def test_extended_pathfinder(self):
+        for method in ['bfw', 'msa', 'fw']:
+            links, car_los, relgap = self._get_extended_roadpathfinder()
+            expected_path_1 = ['z1', 'a', 'b', 'd', 'e', 'z2']
+            self.assertEqual(car_los['path'][0], expected_path_1)
+
+            expected_path_2 = ['z2', 'c', 'b', 'a', 'z1']
+            self.assertEqual(car_los['path'][1], expected_path_2)
+
+    def test_extended_pathfinder_track_link(self):
+        index_dict = self.sm.road_links.reset_index().set_index(['a', 'b'])['index'].to_dict()
+
+        link = index_dict.get(('b', 'd'))
+        assert self.sm.road_links.loc[link, 'a'] == 'b'
+        assert self.sm.road_links.loc[link, 'b'] == 'd'
+        track_links_list = [link]
+        links, car_los, relgap = self._get_extended_roadpathfinder(track_links_list=track_links_list)
+        # expected_path = ['z1', 'a', 'b', 'd', 'e', 'z2']
+        expected_link_path = [*map(index_dict.get, [('a', 'b'), ('b', 'd'), ('d', 'e')])]
+        found_path = links[links[link] == 150]['index'].tolist()
+        self.assertEqual(expected_link_path, found_path)
+
+    def _get_turn_penality_roadpathfinder(self):
+        method = 'bfw'
+        maxiters = 10
+        tolerance = 0.01
+        segments = ['car']
+        time_column = 'time'
+        access_time = 'time'
+        ntleg_penalty = 100
+        turn_penalty = 100
+        turn_penalties = {'rlink_0': ['rlink_4']}
+        network = init_network(self.sm, method, segments, time_column, access_time, ntleg_penalty)
+        volumes = init_volumes(self.sm)
+        links, car_los, relgap_list = extended_roadpathfinder(
+            network,
+            volumes,
+            self.sm.zones,
+            segments=segments,
+            method=method,
+            maxiters=maxiters,
+            tolerance=tolerance,
+            vdf=vdf,
+            log=False,
+            time_col=time_column,
+            zone_penalty=ntleg_penalty,
+            turn_penalty=turn_penalty,
+            track_links_list=[],
+            turn_penalties=turn_penalties,
+            num_cores=num_cores,
+        )
+        return links, car_los, relgap_list
+
+    def test_turn_penality_roadpathfinder(self):
+        links, car_los, relgap = self._get_turn_penality_roadpathfinder()
+        expected_path_1 = ['z1', 'a', 'b', 'c', 'e', 'z2']
+        self.assertEqual(car_los['path'][0], expected_path_1)
+
+        expected_path_2 = ['z2', 'c', 'b', 'a', 'z1']
+        self.assertEqual(car_los['path'][1], expected_path_2)
+
+        expected_columns = ['flow', 'jam_time']
+        for col in expected_columns:
+            self.assertIn(col, links.columns)
