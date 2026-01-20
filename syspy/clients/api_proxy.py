95fc21f0296d341e5bea7ca8e6ca9de739f8748f
Simon Boivin <sboivin@systra.com>
2026-01-20 10:55:07 -0500

    remove api/matrixRoadCaster: moved to quetzal-backend

--- syspy/clients/api_proxy.py
+++ syspy/clients/api_proxy.py
@@ -4,18 +4,17 @@ from time import sleep
 import numpy as np
 import pandas as pd
 import requests
-from shapely.geometry import Point, LineString
-
+from shapely.geometry import LineString
 
 
 def google_maps_encoder(
-        latitude_origin,
-        longitude_origin,
-        latitude_destination,
-        longitude_destination,
-        timestamp=int(time.time()) + 1,
-        token=None,
-        mode='driving'
+    latitude_origin,
+    longitude_origin,
+    latitude_destination,
+    longitude_destination,
+    timestamp=int(time.time()) + 1,
+    token=None,
+    mode='driving',
 ):
     def in_url(coordinates):
         """
@@ -27,16 +26,16 @@ def google_maps_encoder(
         """
         return str(coordinates[1]) + ',' + str(coordinates[0])
 
-    api_url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
-    proto_url = api_url + "origins={0}&destinations={1}"
-    proto_url += "&mode={2}&language=en-EN&sensor=false&departure_time={3}&trafic_model=pessimistic&key={4}"
+    api_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    proto_url = api_url + 'origins={0}&destinations={1}'
+    proto_url += '&mode={2}&language=en-EN&sensor=false&departure_time={3}&trafic_model=pessimistic&key={4}'
 
     url = proto_url.format(
         in_url([longitude_origin, latitude_origin]),
         in_url([longitude_destination, latitude_destination]),
         mode,
         int(timestamp),
-        token
+        token,
     )
     # timestamp must be an int!
     return url
@@ -60,13 +59,11 @@ class Proxy:
 
     example:
     ::
-        # using it with Google Maps (provided by default)
-        # Create proxy instance
+        # using it with Google Maps (provided by default)
+        # Create proxy instance
         data_path = r'api_proxy/'
         conn = sqlite3.connect(data_path + 'google_maps_itineraries.db')
-        proxy = api_proxy.Proxy(
-            conn=conn
-        )
+        proxy = api_proxy.Proxy(conn=conn)
         # Make a request
         data = {
             'latitude_origin': 48.833405,
@@ -74,24 +71,20 @@ class Proxy:
             'latitude_destination': 48.422811,
             'longitude_destination': 2.585593,
             'timestamp': time.time(),
-            'token': token  # You need a valid token here
+            'token': token,  # You need a valid token here
         }
         resp = proxy.get(**data)
     """
+
     def __init__(
         self,
         conn,
         encoder=google_maps_encoder,
         table='json',
-        parameters=(
-            'latitude_origin',
-            'longitude_origin',
-            'latitude_destination',
-            'longitude_destination'
-        ),
+        parameters=('latitude_origin', 'longitude_origin', 'latitude_destination', 'longitude_destination'),
         tolerance=1e-4,
         auto_populate=False,
-        verbose=False
+        verbose=False,
     ):
         """
         Initialise the proxy:
@@ -112,9 +105,7 @@ class Proxy:
             self.local = pd.read_sql(sql, conn)
         except Exception:  # no such table:
             print('create table')
-            empty = pd.DataFrame(
-                index=list(parameters) + ['current', 'json']
-            ).T
+            empty = pd.DataFrame(index=list(parameters) + ['current', 'json']).T
             empty.to_sql(table, conn, index=False, if_exists='fail')
             self.local = pd.read_sql(sql, conn)
             self.local = self.local.astype(np.float)  # todo: not type safe
@@ -143,12 +134,7 @@ class Proxy:
         """
         Create a slicer from the request arguments and the tolerance parameter.
         """
-        return tuple(
-            slice(
-                kwargs[p] - self.tolerance[p],
-                kwargs[p] + self.tolerance[p]
-            ) for p in self.parameters
-        )
+        return tuple(slice(kwargs[p] - self.tolerance[p], kwargs[p] + self.tolerance[p]) for p in self.parameters)
 
     def request_api(self, kwargs):
         """
@@ -169,7 +155,7 @@ class Proxy:
         if self.verbose:
             print('Populate')
         self.populated += 1
-        self.local.loc[self.indexer(kwargs) + (True, )] = resp
+        self.local.loc[self.indexer(kwargs) + (True,)] = resp
         self.clean_index()
 
     def get(self, **kwargs):
@@ -182,7 +168,7 @@ class Proxy:
         try:
             # add 'current' naive slicer
             slices = self.slicer(kwargs) if self.tolerance else self.indexer(kwargs)
-            responses = list(self.local.loc[slices + (slice(None), )])
+            responses = list(self.local.loc[slices + (slice(None),)])
             assert len(responses)
             if self.verbose:
                 print('Result found!')
@@ -200,9 +186,7 @@ class Proxy:
 
     def clean_index(self):
         reset = self.local.reset_index()
-        self.local = reset.set_index(
-            list(self.parameters + ('current',))
-        )['json']
+        self.local = reset.set_index(list(self.parameters + ('current',)))['json']
         self.local.sort_index(inplace=True)
 
     def actual_requests(self):
@@ -215,31 +199,29 @@ class Proxy:
         if self.verbose:
             print('Insert')
         df = self.local.reset_index()
-        df.loc[df['current']].to_sql(
-            self.table,
-            self.conn,
-            if_exists='append',
-            index=False
-        )
+        df.loc[df['current']].to_sql(self.table, self.conn, if_exists='append', index=False)
 
         df['current'] = False
         self.local = df.set_index(list(self.parameters + ('current',)))['json']
 
-def get_distance_matrix(origins, destinations=None, apiKey='', api='here', mode='car',region='polygon', time=None, buffer=0.1,verify=False):
-    '''
+
+def get_distance_matrix(
+    origins, destinations=None, apiKey='', api='here', mode='car', region='polygon', time=None, buffer=0.1, verify=False
+):
+    """
     wrapper that return the time matrix (in seconds) for each OD
     with the Here matrix api or the google matrix api.
 
     parameters
     ----------
     origins (GeoDataframe) = geopandas dataframe with index and geometry (epsg:4326)
-    
+
     destinations (None | GeoDataframe) = geopandas dataframe with index and geometry (epsg:4326)
-    
+
     api (str) ='here', 'google'
-    
+
     apiKey (str) : api key
-    
+
     mode (str) = here : "car" "truck" "pedestrian" "bicycle" "taxi" "scooter" "bus" "privateBus".
     google : driving", "walking", "transit" "bicycling"
 
@@ -256,20 +238,20 @@ def get_distance_matrix(origins, destinations=None, apiKey='', api='here', mode=
     returns
     ----------
     pd.dataframe index: origin, columns: destination. values: time in seconds
-    '''
+    """
     if origins.crs != 'EPSG:4326':
         origins = origins.to_crs(4326)
     df = origins.copy()
     origins_index = df.index.values
     # format geometry to here api format
-    origins = list(df['geometry'].apply(lambda p: {"lat": p.y, "lng": p.x}).values)
+    origins = list(df['geometry'].apply(lambda p: {'lat': p.y, 'lng': p.x}).values)
     # if destination, format them, else: use destination == origin
     if type(destinations) != type(None):
         if destinations.crs != 'EPSG:4326':
             destinations = destinations.to_crs(4326)
         df2 = destinations.copy()
         destinations_index = df2.index.values
-        destinations = list(df2['geometry'].apply(lambda p: {"lat": p.y, "lng": p.x}).values)
+        destinations = list(df2['geometry'].apply(lambda p: {'lat': p.y, 'lng': p.x}).values)
         df = pd.concat([df, df2])
     else:
         destinations = origins
@@ -282,40 +264,40 @@ def get_distance_matrix(origins, destinations=None, apiKey='', api='here', mode=
 
         while not centroid.buffer(buffer).contains(LineString(df['geometry'].values)):
             buffer += 0.1
-            if buffer>1.7:
+            if buffer > 1.7:
                 break
-        polygon = [{"lat": np.round(y,5),"lng": np.round(x,5)} for x, y in list(zip(*centroid.buffer(buffer).exterior.coords.xy))]
+        polygon = [
+            {'lat': np.round(y, 5), 'lng': np.round(x, 5)}
+            for x, y in list(zip(*centroid.buffer(buffer).exterior.coords.xy))
+        ]
         polygon = _remove_duplicates(polygon)
         if region == 'polygon':
-            regionDefinition = {
-                                "type": "polygon",
-                                "outer": polygon,
-                                }
+            regionDefinition = {'type': 'polygon', 'outer': polygon}
         elif region == 'world':
-            regionDefinition =  { "type": "world" }
+            regionDefinition = {'type': 'world'}
         else:
-           raise Exception('{r} is not a valid region. use world or polygon.'.format(r=region))
+            raise Exception('{r} is not a valid region. use world or polygon.'.format(r=region))
         if buffer >= 1.7:
             print('buffer larger than 1.7. region definition set to world as the polygon is most likely')
-            regionDefinition = {"type": "world"}
-        # departureTime : Time of departure at all origins, in ISO 8601 (RFC 3339) 
+            regionDefinition = {'type': 'world'}
+        # departureTime : Time of departure at all origins, in ISO 8601 (RFC 3339)
 
         url = 'https://matrix.router.hereapi.com/v8/matrix?apiKey=' + apiKey + '&async=false'
         body = {
-            "origins": origins,
-            "destinations": destinations,
-            "departureTime": time,
+            'origins': origins,
+            'destinations': destinations,
+            'departureTime': time,
             'transportMode': mode,
-            "regionDefinition": regionDefinition
+            'regionDefinition': regionDefinition,
         }
         try:
-            x = requests.post(url, json=body,verify=verify)
+            x = requests.post(url, json=body, verify=verify)
             resp = json.loads(x.text)
             if x.status_code != 200:
                 raise Exception(resp)
         except:
             sleep(5)
-            x = requests.post(url, json=body,verify=verify)
+            x = requests.post(url, json=body, verify=verify)
             resp = json.loads(x.text)
             if x.status_code != 200:
                 raise Exception(resp)
@@ -323,7 +305,9 @@ def get_distance_matrix(origins, destinations=None, apiKey='', api='here', mode=
         error_index = None
         if resp['matrix'].get('errorCodes') != None:
             if set(resp['matrix']['errorCodes']) != set([0, 3]):
-                errors = np.array([err for err in resp['matrix']['errorCodes']]).reshape(len(origins), len(destinations))
+                errors = np.array([err for err in resp['matrix']['errorCodes']]).reshape(
+                    len(origins), len(destinations)
+                )
                 print('errors', errors)
                 error_index = np.where((errors != 0) & (errors != 3))
         # format response to a dataframa OD with time in secs.
@@ -331,27 +315,28 @@ def get_distance_matrix(origins, destinations=None, apiKey='', api='here', mode=
         if error_index != None:
             mat[error_index] = -9999
             print('times', mat)
-            
+
     elif api == 'google':
         mode = {'car': 'driving', 'pedestrian': 'walking', 'bicycle': 'bicycling'}.get(mode, mode)
-        api_url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
-        proto_url = api_url + "origins={0}&destinations={1}"
-        proto_url += "&mode={2}&language=en-EN&sensor=false&departure_time={3}&trafic_model=pessimistic&key={4}"
+        api_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+        proto_url = api_url + 'origins={0}&destinations={1}'
+        proto_url += '&mode={2}&language=en-EN&sensor=false&departure_time={3}&trafic_model=pessimistic&key={4}'
         url = proto_url.format(
-            '|'.join([str(g['lat'])+ ','+str(g['lng']) for g in origins]),
-            '|'.join([str(g['lat'])+ ','+str(g['lng']) for g in destinations]),
+            '|'.join([str(g['lat']) + ',' + str(g['lng']) for g in origins]),
+            '|'.join([str(g['lat']) + ',' + str(g['lng']) for g in destinations]),
             mode,
             int(time),
-            apiKey)
-        
+            apiKey,
+        )
+
         try:
-            x = requests.get(url, verify=False)
+            x = requests.get(url, verify=verify)
             resp = json.loads(x.text)
             if x.status_code != 200:
                 raise Exception(resp)
         except:
             sleep(5)
-            x = requests.get(url, verify=False)
+            x = requests.get(url, verify=verify)
             resp = json.loads(x.text)
             if x.status_code != 200:
                 raise Exception(resp)
@@ -362,32 +347,33 @@ def get_distance_matrix(origins, destinations=None, apiKey='', api='here', mode=
         mat = np.array(mat).reshape(len(origins), len(destinations))
     else:
         raise Exception('api should be here or google.')
-    
+
     od = pd.DataFrame(mat, index=origins_index, columns=destinations_index)
     od.index.name = 'origin'
     od.columns.name = 'destination'
-    
+
     return od
 
-def get_batches(df,max_od=15):
-    '''
+
+def get_batches(df, max_od=15):
+    """
     gives index to divide dataframe into dataframes of length 15 max
     ex: [[0,15],[15,30],[30,35]] for a df of length 30.
-    '''
-    div = divmod(len(df),max_od)
-    batches = [[max_od*i,max_od*(i+1)] for i in range(div[0])]
-    if div[1]>0:
-        if div[0]==0:
-            batches.append([0,div[1]])
+    """
+    div = divmod(len(df), max_od)
+    batches = [[max_od * i, max_od * (i + 1)] for i in range(div[0])]
+    if div[1] > 0:
+        if div[0] == 0:
+            batches.append([0, div[1]])
         else:
-            batches.append([batches[-1][-1],batches[-1][-1]+div[1]])
+            batches.append([batches[-1][-1], batches[-1][-1] + div[1]])
     return batches
 
 
-def multi_get_distance_matrix(origins,destinations,batch_size=(15,15),api='here',**kwargs):
-    '''
+def multi_get_distance_matrix(origins, destinations, batch_size=(15, 15), api='here', **kwargs):
+    """
     batch api call with 15x15 OD batches (limit per call for here)
-    
+
     parameters
     ----------
     origins (GeoDataframe) = geopandas dataframe with index and geometry (epsg:4326)
@@ -395,35 +381,36 @@ def multi_get_distance_matrix(origins,destinations,batch_size=(15,15),api='here'
     batch_size (tuple) : (origin, destination) batch sizes. (100,1) if 100 ori and 1 des . choices: (15,15), (100,1)
     api (str) : 'here' or 'google'
     **kwargs: get_distance_matrix(origins, destinations, **kwargs)
-    
+
     returns
     ----------
     pd.dataframe index: origin, columns: destination. values: time in seconds
-    '''
+    """
     if api == 'here':
-        assert len(origins)*len(destinations) <= 250_000, 'max 250 000 OD for free HERE api'
+        assert len(origins) * len(destinations) <= 250_000, 'max 250 000 OD for free HERE api'
 
     elif api == 'google':
-        assert len(origins)*len(destinations) <= 40_000, 'max 40 000 OD for free HERE api'
+        assert len(origins) * len(destinations) <= 40_000, 'max 40 000 OD for free HERE api'
     else:
         raise Exception('api should be here or google')
     batches_origins = get_batches(origins, batch_size[0])
     batches_destinations = get_batches(destinations, batch_size[1])
-    mat=pd.DataFrame()
+    mat = pd.DataFrame()
     for batch_o in batches_origins:
-        ori = origins.iloc[batch_o[0]:batch_o[1]]
-        temp_mat=pd.DataFrame()
+        ori = origins.iloc[batch_o[0] : batch_o[1]]
+        temp_mat = pd.DataFrame()
         for batch_d in batches_destinations:
-            des = destinations.iloc[batch_d[0]:batch_d[1]]
+            des = destinations.iloc[batch_d[0] : batch_d[1]]
             try:
-                res = get_distance_matrix(origins=ori,destinations=des,api=api,**kwargs)
+                res = get_distance_matrix(origins=ori, destinations=des, api=api, **kwargs)
             except:
                 sleep(3)
-                res = get_distance_matrix(origins=ori,destinations=des,api=api,**kwargs)
-            temp_mat = pd.concat([temp_mat,res],axis=1)
+                res = get_distance_matrix(origins=ori, destinations=des, api=api, **kwargs)
+            temp_mat = pd.concat([temp_mat, res], axis=1)
             sleep(0.2)
-        mat = pd.concat([mat,temp_mat],axis=0)
+        mat = pd.concat([mat, temp_mat], axis=0)
     return mat
+
 
 def _remove_duplicates(ls):
     seen = {}
