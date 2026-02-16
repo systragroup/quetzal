71715141761845af58f4dba31fed7307717c5d86
Simon Boivin <sboivin@systra.com>
2026-02-16 14:40:29 -0500

    add analysis_pt_time in polars

--- quetzal/polars_engine/analysis.py
+++ quetzal/polars_engine/analysis.py
@@ -1,4 +1,5 @@
 import polars as pl
+import pandas as pd
 from typing import Dict, List, Any
 
 
@@ -14,7 +15,8 @@ def analysis_path(df: pl.DataFrame, vertex_type: Dict[Any, int], excluded: List[
         'boarding_links': pl.col('path').filter((pl.col('type') == 3) & (pl.col('from') != 3)),
         'alighting_links': pl.col('path').filter((pl.col('type') == 3) & (pl.col('to') != 3)),
         'transfers': pl.col('path').filter((pl.col('type') == 2) & (pl.col('from') == 3) & (pl.col('to') == 3)),
-        'ntlegs': pl.concat_list([pl.col('path').head(2), pl.col('path').tail(2)]),
+        'access': pl.col('path').head(2),  # its way simpler to just add 2 columns than having a list of list for ntlegs
+        'eggress': pl.col('path').tail(2),
         'footpaths': pl.concat_list(
             [
                 pl.col('path').filter((pl.col('type') == 2) & (pl.col('to') == 2)),
@@ -54,3 +56,62 @@ def analysis_pt_los(self, walk_on_road: bool = True, excluded: List[str] = []):
 
     to_drop = set(paths_df.columns).union(excluded) - {'index'}  # remove existing columns if already exist (we replace)
     self.pt_los = self.pt_los.drop(to_drop, strict=False).join(paths_df, on='index', maintain_order='left')
+
+
+def analysis_pt_time(self, walk_on_road=False):
+    assert 'boarding_time' in self.links.columns, 'need boarding_time in self.links'
+
+    if walk_on_road:
+        road_links = self.road_links.copy()
+        road_links['time'] = road_links['walk_time']
+        road_to_transit = self.road_to_transit.copy()
+
+        footpaths = pd.concat([road_links, road_to_transit, self.footpaths])
+        access = pd.concat([self.zone_to_road, self.zone_to_transit])
+    else:
+        footpaths = self.footpaths
+        access = self.zone_to_transit
+
+    in_vehicle_time = self.links['time'].to_dict()
+    waiting_time = (self.links['headway'] / 2).to_dict()
+    boarding_time = self.links['boarding_time'].to_dict()
+
+    exprs = {
+        'in_vehicle_time': pl.col('link_path').list.eval(pl.element().replace(in_vehicle_time, default=0)).list.sum(),
+        'waiting_time': pl.col('boarding_links').list.eval(pl.element().replace(waiting_time, default=0)).list.sum(),
+        'boarding_time': pl.col('boarding_links').list.eval(pl.element().replace(boarding_time, default=0)).list.sum(),
+    }
+    # drop time columns if they exist
+    to_drop = ['in_vehicle_time', 'waiting_time', 'boarding_time', 'footpath_time', 'access_time']
+    self.pt_los = self.pt_los.drop(to_drop, strict=False)
+    los = self.pt_los.lazy()
+    # compute  new times columns
+    los = los.with_columns(**exprs)
+    # compute footpath_time and access_time
+
+    # for tuple paths, we need to explode first and join our time on a,b
+    footpaths_time = pl.LazyFrame(footpaths[['a', 'b', 'time']])
+    _los = los.select(['index', 'footpaths'])
+    _los = _los.explode('footpaths')
+    _los = _los.with_columns(
+        [pl.col('footpaths').list.get(0).alias('a'), pl.col('footpaths').list.get(1).alias('b')]
+    ).drop('footpaths')
+    _los = _los.join(footpaths_time, on=['a', 'b'])
+    _los = _los.group_by('index').agg(pl.col('time').sum().alias('footpath_time'))
+    los = los.join(_los, on='index', maintain_order='left', how='left')
+
+    # compute ntlegs time as access_time = access_time + eggress_time
+    ntlegs_time = pl.LazyFrame(access[['a', 'b', 'time']])
+    _los = los.select(['index', 'access', 'eggress'])
+    # split access to a,b columns and join the access_time on a,b
+    _los = _los.with_columns([pl.col('access').list.first().alias('a'), pl.col('access').list.last().alias('b')])
+    _los = _los.join(ntlegs_time, on=['a', 'b']).drop(['a', 'b']).rename({'time': 'access_time'})
+    # split eggress to a,b columns and join the egress_time on a,b
+    _los = _los.with_columns([pl.col('eggress').list.first().alias('a'), pl.col('eggress').list.last().alias('b')])
+    _los = _los.join(ntlegs_time, on=['a', 'b']).drop(['a', 'b']).rename({'time': 'eggress_time'})
+    # access_time = access + eggress
+    _los = _los.with_columns((pl.col('access_time') + pl.col('eggress_time')).alias('access_time'))
+    # add total access_time to the los
+    los = los.join(_los.select(['index', 'access_time']), on='index', maintain_order='left', how='left')
+
+    self.pt_los = los.collect(engine='streaming')
