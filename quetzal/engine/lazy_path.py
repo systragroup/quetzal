3bb3f546638514e665a632f4a0cf51f666028cba
Simon Boivin <sboivin@systra.com>
2026-02-26 11:39:52 -0500

    lazyPaths. predecessor in 2d. we concat instead of adding a dimension. so we can reuse the same normal function for get_paths and all

--- quetzal/engine/lazy_path.py
+++ quetzal/engine/lazy_path.py
@@ -1,30 +1,28 @@
 import numpy as np
+import pandas as pd
 from typing import List, Dict, Union
-from quetzal.engine.fast_utils import compute_offsets, remap_int_array, dict_to_lut
-from quetzal.engine.lazy_path_utils import (
-    get_path_3d,
-    remap_predecessor,
+from quetzal.engine.fast_utils import (
+    compute_offsets,
+    remap_int_array,
+    dict_to_lut,
+    get_path,
     compute_path_lengths,
     get_flat_path,
-    get_paths_hash,
-    sum_jagged_array,
-    analysis_flat_path,
 )
-
-import pandas as pd
+from quetzal.engine.lazy_path_utils import get_paths_hash, sum_jagged_array, analysis_flat_path
 
 
 class LazyPaths:
     def __init__(self, predecessors, node_index: Dict[str, int], sources: List[str], reverse: bool = False) -> int:
-        # TODO: append predecessor to stay 2d? this way we can reuse the same functions.
-        # just need to change source_index. thi also allow us to have different source per pathfinder Session.
-        self.predecessors = np.array([predecessors])
+        self.predecessors = predecessors
         self.node_index = node_index  # {link_0: 0, link_1: 1, ...}
-        self.sources = sources  # list of zones
         self.reverse = reverse
 
         self.index_node = {k: v for v, k in node_index.items()}  # {0: link_0, 1: link1, ...}
         self.source_index = dict(zip(sources, range(len(sources))))  # {zone_0:0, zone_1: 1, ...}
+        # when there are multiple pathfinder session. concat predecessor on origin (axis=0)
+        # so we need to add an offset, ex: session 2 first origin will be 1200 and not 0 because its predecessor start at column 1200
+        self.source_offset = {0: 0}  # {session_id: offset}
 
     def __repr__(self):
         return f'Lazy paths with {self.predecessors.shape[0]} sessions'
@@ -32,20 +30,20 @@ class LazyPaths:
     def __hash__(self):
         return id(self)
 
-    def append(self, predecessor, index_node) -> int:
-
-        new_pred = remap_predecessor(self.predecessors[0], self.node_index, predecessor, index_node)
-        self.predecessors = np.vstack([self.predecessors, [new_pred]])
+    def append(self, predecessor, index_node, session_id: int) -> int:
+        new_pred = remap_predecessor(self.predecessors, self.node_index, predecessor, index_node)
+        self.source_offset[session_id] = len(self.predecessors)
+        self.predecessors = np.concatenate([self.predecessors, new_pred], axis=0)
         return self.predecessors.shape[0]
 
-    def _get_int_path(self, o: int, d: int, s: int) -> List[int]:
-        return get_path_3d(self.predecessors, o, d, s, self.reverse)
+    def _get_int_path(self, o: int, d: int) -> List[int]:
+        return get_path(self.predecessors, o, d, self.reverse)
 
     def _remap_od_list(self, od_list: np.ndarray) -> np.ndarray[int, int]:
-        o_list = [*map(self.source_index.get, od_list[:, 0])]
-        d_list = [*map(self.node_index.get, od_list[:, 1])]
-        s_list = np.array(od_list[:, 2], dtype=np.int32)
-        od_index_list = np.stack([o_list, d_list, s_list], axis=1)
+        o_list = np.array([*map(self.source_index.get, od_list[:, 0])], dtype=np.int32)
+        d_list = np.array([*map(self.node_index.get, od_list[:, 1])], dtype=np.int32)
+        offset = np.array([*map(self.source_offset.get, od_list[:, 2])], dtype=np.int32)
+        od_index_list = np.stack([o_list + offset, d_list], axis=1)
         return od_index_list
 
     def _get_jagged_int_path(self, od_index_list: np.ndarray):
@@ -58,7 +56,7 @@ class LazyPaths:
     # exposed
     #
     def get_path(self, origin: str, destination: str, session: int, remap: bool = True) -> Union[List[str], List[int]]:
-        path = self._get_int_path(self.source_index[origin], self.node_index[destination], session)
+        path = self._get_int_path(self.source_index[origin] + self.source_offset[session], self.node_index[destination])
         if remap:
             path = [*map(self.index_node.get, path)]
         return path
@@ -90,6 +88,28 @@ class LazyPaths:
         return sum_jagged_array(flat_paths, offsets)
 
 
+def remap_predecessor(full_pred, full_dict, pruned_pred, pruned_dict):
+    # return pruned_pred in the indexing of full_pred (row-wise)
+    pruned_pred = pruned_pred.copy()
+    pruned_to_full = {k: full_dict[v] for k, v in pruned_dict.items()}
+
+    # create a lookup table :  prune to full index
+    pruned_to_full_map = dict_to_lut(pruned_to_full)
+    # remap value in pruned_predecessor to the one in full_pred
+    mask = pruned_pred != -9999  # need to mask if we want to keep -9999,else it will look at index -9999 in lut
+    pruned_pred[mask] = pruned_to_full_map[pruned_pred[mask]]
+
+    # reorder rows to the full predecessor indexing. (and shape it as the full_pred rows.)
+    col = pruned_pred.shape[0]
+    rows = full_pred.shape[1]
+    new_predecessor = np.full((col, rows), -9999, dtype=np.int32)
+    full_indexes = np.array(list(pruned_to_full.values()))
+    pruned_indexes = np.array(list(pruned_to_full.keys()))
+    new_predecessor[:, full_indexes] = pruned_pred[:, pruned_indexes]
+
+    return new_predecessor
+
+
 def concat_lazy_los(los_list) -> tuple[pd.DataFrame, LazyPaths]:
     # concat Lazy_paths object in a single one
     # remove lazy_paths of los
@@ -100,7 +120,8 @@ def concat_lazy_los(los_list) -> tuple[pd.DataFrame, LazyPaths]:
     pt_los['lazy_session'] = pt_los['path'].map(lazy_session)
     lazy_path = lazy_path_list[0]
     for obj in lazy_path_list[1:]:
-        lazy_path.append(obj.predecessors[0], obj.index_node)
+        session_id = lazy_session.get(obj)
+        lazy_path.append(obj.predecessors, obj.index_node, session_id)
     pt_los = pt_los.drop(columns='path')
     return pt_los, lazy_path
 
