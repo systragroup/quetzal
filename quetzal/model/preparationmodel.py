fcab66d22ff591de60320ed3f69b18e252216761
Simon Boivin <sboivin@systra.com>
2026-09-25 10:33:59 -0400

    fixe in preparation model

--- quetzal/model/preparationmodel.py
+++ quetzal/model/preparationmodel.py
@@ -647,12 +647,9 @@ class PreparationModel(model.Model, cubemodel.cubeModel):
                 num_cores=num_cores,
                 **kwargs,
             )
-        # we added a first node to to the mapmatching. we need to reshift the index
-        # a link is 2 node. if we have 5 links, there are 6 points in mapmatching.
+        # we added the last node. shift to go back on links
         matched_links = matched_links.merge(matched_links.shift(-1), on=['index', 'trip_id'], suffixes=['_a', '_b'])
 
-        matched_links['road_id_a'] = matched_links['road_id_a'].apply(lambda x: road_links.links_index_dict.get(x))
-        matched_links['road_id_b'] = matched_links['road_id_b'].apply(lambda x: road_links.links_index_dict.get(x))
         road_a_dict = matched_links['road_id_a'].to_dict()
         road_b_dict = matched_links['road_id_b'].to_dict()
         offset_a_dict = matched_links['offset_a'].to_dict()
